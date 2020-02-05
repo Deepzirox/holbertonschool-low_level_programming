8a96ea2f06292f972ecb636fd90b5d33fef3842d
Andres Tobon <toboncamilo33@gmail.com>
2020-02-05 15:39:04 -0500

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,13 @@
 /**
  * main - main function
  *
- * Return: (0)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-printf("%s \n", "with proper grammar, but the outcome is a piece of art,");
+printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
+
 
