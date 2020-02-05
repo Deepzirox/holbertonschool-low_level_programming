eedb42512fcd3c1eca57cd932fe032d4a735334d
Andres Tobon <toboncamilo33@gmail.com>
2020-02-05 15:04:12 -0500

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-/*
-HELLO WORLD IN C
-*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
