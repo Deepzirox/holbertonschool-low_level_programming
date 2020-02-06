a0fdb9d8c830fdf7bd231df69837e18a96349647
Andres Tobon <toboncamilo33@gmail.com>
2020-02-06 16:03:48 -0500

    adding file 2

---
+++ 0x01-variables_if_else_while/2-printf_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	for (int i = 'a'; i <= 'z'; i++)
+	putchar(i);
+
+	putchar('\n');
+	return (0);
+}
