f9c15c562fe0dd4d7ce784ed0c3198ab1ff5d878
Andres Tobon <toboncamilo33@gmail.com>
2020-02-06 18:09:06 -0500

    adding file 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+ * main - Entry point
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+	putchar((int)alpha);
+	alpha++;
+	}
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+	putchar((int)toupper(alpha));
+	alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
