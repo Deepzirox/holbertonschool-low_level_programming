489ea6bded248bf5a20492547386df2807838d26
Andres Tobon <toboncamilo33@gmail.com>
2020-02-06 13:17:41 -0500

    4

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,16 +4,15 @@
  * main - Entry point
  * Return: 0 Always (Success)
  */
-#define neutral 0
 int main(void)
 {
 srand(time(0));
 long n = rand() - RAND_MAX / 2;
-if (n > neutral)
+if (n > 0)
 {
 printf("%i %s \n", n, "is positive");
 }
-else if (n == neutral)
+else if (n == 0)
 {
 printf("%i %s \n", n, "is zero");
 }
