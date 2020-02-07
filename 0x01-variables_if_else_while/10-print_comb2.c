4a8b4ace21d983d3af06117ae2a19602026825e9
Andres Tobon <toboncamilo33@gmail.com>
2020-02-06 21:39:25 -0500

    adding file

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+* main - Entry point
+* Return: 0 Always (Success)
+*/
+int main(void)
+{
+int iterator;
+while (iterator < 100)
+{
+putchar((iterator/10)+'0');
+putchar((iterator%10)+'0');
+if(iterator != 99)
+{
+putchar(44);
+putchar(32);
+}
+iterator++;
+}
+putchar('\n');
+return (0);
+}
+
