494217f147bc79680f624bcb62319c8c2fa72014
Andres tobon <1611@holbertonschool.com>
2020-02-11 08:11:20 -0500

    adding file

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 #include "holberton.h"
+/**
+ * main- Entry point for app
+ * Return: 0 Always (Success)
+ */
 
-int main()
+
+int main(void)
 {
 	char hol[9] = "Holberton";
+
 	for (int i = 0; i <= 9; i++)
 	{
 		_putchar(hol[i]);
