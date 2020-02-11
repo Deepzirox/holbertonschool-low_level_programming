49fa2b564b2ae45dd33fd4775bd7dd44b65b4427
Andres tobon <1611@holbertonschool.com>
2020-02-11 17:41:58 -0500

    adding file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * _isalpha - check if a char is lowercase
+ * @c: char to check
+ * Return: 1 (lowercase), 0 (otherwise)
+ */
+int _isalpha(int c)
+{
+	char upper;
+	char lower;
+	char value;
+	int res;
+
+	value = c;
+	res = 0;
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		if (lower == value)
+		{
+			res = 1;
+		}
+	}
+	for (upper = 'A'; upper <= 'Z'; upper++)
+	{
+		if (upper == value)
+		{
+			res = 1;
+		}
+
+	}
+	return (res);
+
+}
