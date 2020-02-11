e32a15bed45c5ee57419cc58d99fb6a698960193
Andres tobon <1611@holbertonschool.com>
2020-02-11 01:04:25 -0500

    adding proyect

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "holberton.h"
+
+int main()
+{
+	char hol[9] = "Holberton";
+	for (int i = 0; i <= 9; i++)
+	{
+		_putchar(hol[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,5 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include "_putchar.c"
+int _putchar(char character);
+#endif
