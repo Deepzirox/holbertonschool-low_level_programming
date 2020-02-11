5951c480c93830e6b90e82a251f2fadd57ab5b21
Andres tobon <1611@holbertonschool.com>
2020-02-11 14:13:40 -0500

    adding file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include "holberton.h"
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ * @void: no parameters
+ * Return: None
+ */
 void print_alphabet_x10(void)
 {
-	for(int cicle = 0; cicle < 10; cicle++)
+	for (int cicle = 0; cicle < 10; cicle++)
 	{
-		for(int ch = 'a'; ch <= 'z'; ch++)
+		for (int ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}
