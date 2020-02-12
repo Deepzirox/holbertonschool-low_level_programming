e1a2e15e26b64ada62531f9c8fe6b0aa08301186
Andres tobon <1611@holbertonschool.com>
2020-02-11 21:22:08 -0500

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,14 @@
 #include "holberton.h"
-
+/**
+ * print_last_digit - print and return the las digit of integer value
+ * @n: integer num
+ * Return: int (always)
+ */
 int print_last_digit(int n)
 {
 	int res;
 	char resc;
+
 	res = _abs((n % 10));
 	resc = res + 48;
 	_putchar(resc);
