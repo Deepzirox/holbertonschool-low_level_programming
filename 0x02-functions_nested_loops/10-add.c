3d1f83ff5db0b83a28734710f61f90975bcb032e
Andres tobon <1611@holbertonschool.com>
2020-02-12 21:53:22 -0500

    adding file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+include "holberton.h"
+/**
+ * add - get sum of two integers
+ * @num1: integer value
+ * @num2: integer value
+ * Return: (num1 + num) Always
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
