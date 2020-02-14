b1325a9c5fcbe154472a13cea6afc56f09c21a26
Andres tobon <1611@holbertonschool.com>
2020-02-14 00:36:01 -0500

    adding file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * print_to_98 - print n to 98
+ * @n: integer
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%i ", i);
+	}
+}
