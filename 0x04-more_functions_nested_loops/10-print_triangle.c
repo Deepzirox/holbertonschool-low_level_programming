25c52759107467eb7c7b2dea27da34ea9d60a1c6
Andres tobon <1611@holbertonschool.com>
2020-02-14 00:18:58 -0500

    adding file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_triangle - print a triangle in the size given
+ * @size: lenght of triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, i2, i3, dep, ped;
+
+	dep = size;
+	ped = 0;
+	for (i = 0; i < size; i++)
+	{
+		for (i2 = 0; i2 < dep - 1; i2++)
+		{
+			_putchar(' ');
+		}
+		ped++;
+		for (i3 = 0; i3 < ped; i3++)
+		{
+			_putchar('#');
+		}
+		dep--;
+		_putchar('\n');
+	}
+}
