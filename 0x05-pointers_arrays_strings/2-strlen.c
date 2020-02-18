677c23bd2eebb59bb528750b00c0a6e4e1bff2d4
Andres Camilo <1611@holbertonschool.com>
2020-02-18 14:37:37 -0500

    adding file

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,14 @@
 #include "holberton.h"
-
+/**
+ * _strlen - lenght of string
+ * @s: string
+ * Return: int (Success)
+ */
 int _strlen(char *s)
 {
 	int c;
 
-	while(s[c] != '\0')
+	while (s[c] != '\0')
 		c++;
 
 	return (c);
