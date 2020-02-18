d42dab64d9c4e4d73f1b0c8b2f2da55594647826
Andres Camilo <1611@holbertonschool.com>
2020-02-18 15:18:34 -0500

    fix

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_rev - print reverse string
+ * @s: string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int c,cc;
+
+	while(s[c] != '\0')
+	{
+		c++;
+	}
+	for (cc = c; cc >= 0; cc--)
+	{
+		_putchar(s[cc]);
+	}
+	_putchar(10);
+}
