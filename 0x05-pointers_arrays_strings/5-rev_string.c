a16fa1a84ca6d210ecd2b97bee1639e1d730d939
Andres Camilo <1611@holbertonschool.com>
2020-02-19 17:47:35 -0500

    fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,16 +9,14 @@ void rev_string(char *s)
 	char handler[1000];
 	int c1, c2, lenght, point;
 
-	c2 = 0;
 	lenght = 0;
 	point = 0;
 
-	while (s[c1] != '\0')
+	for (c1 = 0; s[c1] != '\0'; c1++)
 	{
 		handler[c1] = s[c1];
-		c1++;
 		lenght++;
-	}
+	}	
 	for (point = lenght - 1; point >= 0; point--)
 	{
 		s[point] = handler[c2];
