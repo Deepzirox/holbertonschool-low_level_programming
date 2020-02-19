c6201ccd9601be255e1357ab6c8c9fa51f025b2a
Andres Camilo <1611@holbertonschool.com>
2020-02-18 21:50:13 -0500

    adding file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,20 @@
 #include "holberton.h"
-
-void puts(char *str)
+/**
+ * puts2 - print string in pair
+ * @str: string
+ * Return: void
+ */
+void puts2(char *str)
 {
+	int i, pair;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (pair % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		pair++;
+	}
+	_putchar(10);	
 }
