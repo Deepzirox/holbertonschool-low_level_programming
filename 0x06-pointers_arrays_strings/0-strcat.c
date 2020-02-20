4efe8c0e163c74a0da6dd9b5a36f6b32dad8ed36
Andres Camilo <1611@holbertonschool.com>
2020-02-20 13:20:58 -0500

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,24 @@
 #include "holberton.h"
-
+/**
+ * _strcat - concatenate two variables
+ * @dest: append destiny
+ * @src: append source
+ * Return: if True (dest+source)
+ */
 char *_strcat(char *dest, char *src)
 {
 	int i, i2;
 
 	i = 0;
-	while(dest[i] != '\0')
-	{	
+	while (dest[i] != '\0')
+	{
 		i++;
 	}
-	
+
 	i2 = 0;
 	while ((dest[i++] = src[i2++]) != '\0')
 	{}
 
 	dest[i] = '\0';
-	return (dest);	
+	return (dest);
 }
