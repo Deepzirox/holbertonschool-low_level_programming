1a5371b2ff6904973fc39ef7bfe982a99c1f04c5
Andres Camilo <1611@holbertonschool.com>
2020-02-20 14:15:32 -0500

    fix

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+        int i, i2;
+
+        i = 0;
+        while (dest[i] != '\0')
+        {
+                i++;
+        }
+        i2 = 0;
+        while ((dest[i++] = src[i2++]))
+        {
+		if (i >= n)
+		{
+			break;
+		}
+	}
+	if (i < n)
+	{
+		dest[i] = '\0';
+	}
+        return (dest);
+}
