199c16183ae9210f66168c078ab09f61517f7dcc
Andres Camilo <1611@holbertonschool.com>
2020-02-20 19:31:11 -0500

    fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,23 @@
 #include "holberton.h"
-
+/**
+ * _strncpy - copy string
+ * @dest: destination
+ * @src: source
+ * @n: int
+ * Return: character
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 	
-	for (i = 0; (i < n); i++)
+	i = 0;
+	for(; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
