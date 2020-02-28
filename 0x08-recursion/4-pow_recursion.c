7b745ba024d748e37f272fc52186072ce9a45af8
Andres Camilo <1611@holbertonschool.com>
2020-02-28 13:53:53 -0500

    fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,16 +1,21 @@
 #include "holberton.h"
-
+/**
+ * _pow_recursion - find power of number
+ * @x: int
+ * @y: int
+ * Return: power is succes -1 if not valid
+ */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return -1;
+		return (-1);
 
 	if (y == 0)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return x * _pow_recursion(x, y - 1);
-	}	
+		return (x * _pow_recursion(x, y - 1));
+	}
 }
