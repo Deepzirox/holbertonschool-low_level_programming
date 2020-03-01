e2d8c541668342161c385904a90761f12591b0f9
Andres Camilo <1611@holbertonschool.com>
2020-03-01 17:27:11 -0500

    add

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point program
+ * @argv: parameters
+ * @argc: number of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, a, b, r;
+	char *err = "Error";
+
+	if (argc != 3)
+	{
+		printf("%s\n", err);
+	}
+	else
+	{
+		a = atoi(*(argv + 1));
+		b = atoi(*(argv + 2));
+		r = a * b;
+		printf("%d\n", r);
+	}
+
+	return (0);
+}
