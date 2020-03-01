32afc8fa180628264026de554996450e22da47aa
Andres Camilo <1611@holbertonschool.com>
2020-03-01 12:07:59 -0500

    fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point program
+ * @argv: parameters
  * @argc: number of arguments
  * Return: 0
  */
-int main(int argc)
+int main(int argc, char *argv[])
 {
-
+	(void)*argv;
 	printf("%d\n", argc - 1);
 	return (0);
 }
