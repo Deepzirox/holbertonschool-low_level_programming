db9e00d18424da06ea3b43c0fffefc0e3b8a0315
Andres Camilo <1611@holbertonschool.com>
2020-03-02 00:24:13 -0500

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * sum - validate char and add
+ * @ch: char to evaluate
+ * @tmp: support that contains command
+ * @res: result to print
+ * Return: void
+ */
+void sum(char **ch, char *tmp, int *res)
+{
+	char tm = **ch;
+
+	int validator = isdigit(tm);
+
+	if (validator == 0)
+	{
+		printf("Error\n");
+		exit(0);
+	}
+	else
+	{
+		*res += atoi(tmp);
+	}
+}
+/**
+ * main - entry point program
+ * @argc: num arguments
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+	int i, res2 = 0;
+	char *tmp;
+
+	argv++;
+	if (argc > 0)
+	{
+		for (i = 0; i < argc - 1; i++, argv++)
+		{
+			tmp = *argv;
+			for (; *tmp != '\0'; tmp++)
+				sum(&tmp, tmp, &res2);
+		}
+		printf("%d \n", res2);
+	}
+	return (0);
+}
