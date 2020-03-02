33ce727c13e9fb01848a167cbdd718c6f2f089a4
Andres Camilo <1611@holbertonschool.com>
2020-03-02 12:38:39 -0500

    fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
+/**
+ * _isdigit - check if a char is uppercase
+ *@c: char to check
+ * Return: 1 (uppercase), 0 (otherwise)
+ */
+int _isdigit(int c)
+{
+	int value, res;
+
+	res = 0;
+	for (value = '0'; value <= '9'; value++)
+	{
+		if (value == c)
+		{
+			res = 1;
+		}
+	}
+
+	return (res);
+}
 /**
  * sum - validate char and add
  * @ch: char to evaluate
@@ -12,7 +31,7 @@ void sum(char **ch, char *tmp, int *res)
 {
 	char tm = **ch;
 
-	int validator = isdigit(tm);
+	int validator = _isdigit(tm);
 
 	if (validator == 0)
 	{
@@ -32,7 +51,6 @@ void sum(char **ch, char *tmp, int *res)
  */
 int main(int argc, char *argv[])
 {
-
 	int i, res2 = 0;
 	char *tmp = 0;
 	
