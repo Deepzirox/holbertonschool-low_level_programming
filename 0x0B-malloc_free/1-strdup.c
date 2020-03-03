9eb37cde7b3d1af2217342b0c6217a7dd6036e7a
Your Name <you@example.com>
2020-03-03 21:54:18 +0000

    fix

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * _strdup - copy string to new memory
+ * @str: string to copy
+ * Return: pointer to new string
+ */
+char *_strdup(char *str)
+{
+	char *aux;
+	int counter = 0, i;
+
+	while (str[counter] != '\0')
+		counter++;
+
+	counter += 1;
+	aux = malloc(counter * sizeof(char));
+	if (aux == NULL || counter == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < counter; i++)
+		{
+			aux[i] = str[i];
+		}
+	}
+	return (aux);
+}
