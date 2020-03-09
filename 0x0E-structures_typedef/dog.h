fbf2e2b9b7931923d7963b19463db9b1b7118a6a
Andres Camilo <1611@holbertonschool.com>
2020-03-09 13:32:44 -0500

    add

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H_
+#define DOG_H_
+/**
+ * struct dog - Django
+ * @name: pet name
+ * @age: pet age
+ * @owner: pet owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
