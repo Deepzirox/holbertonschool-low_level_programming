ad4b68da9b70c03cf8100bbbe782b3b0711b03c5
Andres Camilo <1611@holbertonschool.com>
2020-03-09 16:06:26 -0500

    fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,14 +17,7 @@ void print_dog(struct dog *d)
 
 
 		printf("Name: %s\n", d->name);
-		if (d->age == 0)
-			printf("Age: %s\n", "(nil)");
-		else
-			printf("Age: %f\n", d->age);
+		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner);
-	}
-	else
-	{
-		printf(" ");
 	}
 }
