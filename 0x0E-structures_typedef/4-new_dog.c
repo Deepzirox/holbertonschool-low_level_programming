5a8d39609c0f455530e7a380cfb930220558a4a9
Andres Camilo <1611@holbertonschool.com>
2020-03-09 22:59:08 -0500

    fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,21 +14,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
-		return (NULL);
-	if (name == 0)
-	{
-		free(owner);
+	{	
 		free(new);
 		return (NULL);
 	}
-	if (owner == 0)
+	else
 	{
-		free(name);
-		free(new);
-		return (NULL);
+		(*new).name = name;
+		(*new).age = age;
+		(*new).owner = owner;
 	}
-	(*new).name = name;
-	(*new).age = age;
-	(*new).owner = owner;
 	return (new);
 }
