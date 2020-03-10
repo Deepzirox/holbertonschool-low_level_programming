cdda3414d988bfd7d549c47842351ee93ba92154
Andres Camilo <1611@holbertonschool.com>
2020-03-09 21:48:56 -0500

    fix

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	(*dog_t).name = name;
+	(*dog_t).age = age;
+	(*dog_t).owner = owner;
+}
