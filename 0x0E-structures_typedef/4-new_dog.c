e0aecd2368555e0f90d09369bf1f841958a508ac
Andres Camilo <1611@holbertonschool.com>
2020-03-10 22:28:47 -0500

    add

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,52 +6,21 @@
  * @s: string
  * Return: pointer to address
  */
-char *allocate(char *s)
-{
-	char *alloc;
-	int it;
-
-	if (s == 0)
-		return (NULL);
-
-	for (it = 0; s[it] != '\0'; it++)
-	{
-	}
-	alloc = malloc(sizeof(char) * 1);
-	if (alloc == NULL)
-		return (NULL);
-
-	for (it = 0; s[it] != '\0'; it++)
-	{
-		alloc[it] = s[it];
-	}
-	alloc[it] = '\0';
-	return (alloc);
-}
-/**
- * new_dog - create new dog
- * @name: pet name
- * @age: pet age
- * @owner: pet owner
- * Return: pointer to struct
- */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_pet;
 
 	new_pet = malloc(sizeof(dog_t));
 	if (new_pet == 0)
-		return (NULL);
+		return (0);
 
-	new_pet->name = allocate(name);
-	new_pet->owner = allocate(owner);
-	if (new_pet->name == 0 || new_pet->owner == 0)
+	if ((name == 0 || owner == 0))
 	{
-		free(new_pet->name);
-		free(new_pet->owner);
 		free(new_pet);
 		return (NULL);
 	}
+	new_pet->owner = owner;
+	new_pet->name = name;
 	new_pet->age = age;
 	return (new_pet);
 }
