1300ad1594a235ede866afecd716959207d9f4e2
Andres Camilo <1611@holbertonschool.com>
2020-03-20 11:17:34 -0500

    add

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -25,7 +25,8 @@ int len(const char *str)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
-
+	if (new_node == NULL)
+		return (NULL);
 	new_node->str = strdup(str);
 	new_node->len = len(str);
 	new_node->next = *head;
