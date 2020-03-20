6c70ceaaabf374beffccfb71993eaf6b145e3642
Andres Camilo <1611@holbertonschool.com>
2020-03-19 21:47:56 -0500

    adding singly list proyect

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_list - print all nodes
+ * @h: list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	char *s = 0, *v;
+	int i = 0;
+
+	while (h != NULL)
+	{
+		s = h->str;
+		if (s == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, s);
+
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
