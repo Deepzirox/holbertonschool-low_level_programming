9f76ec92e8d7c6f4bdd5b59629f15524d26dcc97
Andres Camilo <1611@holbertonschool.com>
2020-03-20 11:16:06 -0500

    add

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+	* len - get lenght of string
+	* @str: string
+	* Return: lenght of str
+	*/
+int len(const char *str)
+{
+	int counter = 0;
+
+	while (str[counter] != '\0')
+		counter++;
+
+	return (counter);
+}
+/**
+	* add_node - adding new node
+	* @head: head list
+	* @str: string
+	* Return: address of new node
+	*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+
+	new_node->str = strdup(str);
+	new_node->len = len(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
