a19a52fb31dea825ffd29819c113c9b6bd539da3
Andres Camilo <1611@holbertonschool.com>
2020-03-24 18:01:57 -0500

    adding file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - free head of list
+ * @head: head
+ * Return: voidp
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (*head == NULL || head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
