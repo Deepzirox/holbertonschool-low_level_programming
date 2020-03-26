903208b3a00f24a2f060e21134a5b5dc5d007b2a
Andres Camilo <1611@holbertonschool.com>
2020-03-26 18:02:36 -0500

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,66 +1,28 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
-	* _isdigit - check if contain only digits
-	* @c: string
-	* Return: 1 if digit 0 is not
-	*/
-int _isdigit(const char *c)
-{
-	int value, res = 1;
-
-	for (value = 0; c[value] != '\0'; value++)
-	{
-		if (c[value] != '0' && c[value] != '1')
-		{
-			return (0);
-		}
-	}
-
-	return (res);
-}
-/**
-	* ctoint - convert string to int
-	* @string: string
-	* Return: result number
-	*/
-unsigned int ctoint(const char *string)
-{
-		int n = 0;
-
-		while ((*string >= '0') && (*string <= '9'))
-		{
-			n = (n * 10) + (*string - '0');
-			string++;
-		}
-		return (n);
-}
-/**
 	* binary_to_uint - convert integer to binary
 	* @b: string
 	* Return: result number
 	*/
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int value;
-	int reminder, base = 1;
-	unsigned int res = 0;
-	int check;
+	unsigned int value = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	check = _isdigit(b);
-	if (!check)
-		return (0);
-
-	value = ctoint(b);
-	while (value != 0)
+	while (*b != '\0')
 	{
-		reminder = value % 10;
-		res = res + reminder * base;
-		value = value / 10;
-		base = base * 2;
+		if (*b != '0' && *b != '1')
+		{
+			return (0);
+		}
+		value <<= 1;
+		if (*b == '1')
+			value++;
+
+		b++;
 	}
-	return (res);
+	return (value);
 }
