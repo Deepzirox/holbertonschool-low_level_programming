a363b2071307bcc2d72c1408a2ca3c91d6957044
Andres Camilo <1611@holbertonschool.com>
2020-03-26 19:37:50 -0500

    please

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+	* print_binary - print decimal to binary
+	* @n: decimal
+	* Return: void
+	*/
+void print_binary(unsigned long int n)
+{
+
+	int boolean = 0, i, bin;
+
+	if (!n)
+	{
+		_putchar('0');
+		return;
+	}
+	for (i = 31; i >= 0; i--)
+	{
+		bin = n >> i;
+		if ((bin & 1) && !boolean)
+			boolean = 1;
+
+		if (boolean)
+		{
+			if (bin & 1)
+				_putchar('1');
+			else
+				_putchar('0');
+		}
+	}
+	_putchar('\0');
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,4 +1,6 @@
 #ifndef HOL
 #define HOL
+int _putchar(char character);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 #endif
