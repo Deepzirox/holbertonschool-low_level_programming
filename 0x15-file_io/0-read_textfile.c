a918357b31605e91cf12e301be0e5983fc38e063
Andres Camilo <1611@holbertonschool.com>
2020-03-31 16:19:39 -0500

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,31 +3,36 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+/**
+ * read_textfile - read file content
+ * @filename: name of file
+ * @letters: bytes to read
+ * Return: count of printed bytes
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int fd;
-  ssize_t total = 0, validate = 0, x = letters;
-  char *buffer = malloc(sizeof(char *) * letters);
+	int fd;
+	ssize_t total = 0, validate = 0;
+	char *buffer = malloc(sizeof(char *) * letters);
 
-  if (!buffer || !filename)
-  {
-    free(buffer);
-    return (0);
-  }
-  fd = open(filename, O_RDONLY);
-  if(fd == -1)
-  {
-    free(buffer);
-    return (0);
-  }
-  total = read(fd, buffer, letters);
-  validate = write(STDOUT_FILENO, buffer, total);
-  if (validate != total)
-  {
-    free(buffer);
-    return (0);
-  }
-  free(buffer);
-  return (total);
+	if (!buffer || !filename)
+	{
+		free(buffer);
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	total = read(fd, buffer, letters);
+	validate = write(STDOUT_FILENO, buffer, total);
+	if (validate != total)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	return (validate);
 }
