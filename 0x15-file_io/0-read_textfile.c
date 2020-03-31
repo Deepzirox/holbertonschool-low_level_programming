ccf346a4c85f9d3ebb3a5e86608af572c7c3b99e
Andres Camilo <1611@holbertonschool.com>
2020-03-31 16:42:37 -0500

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -27,7 +27,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	total = read(fd, buffer, letters);
+	if (total == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 	validate = write(STDOUT_FILENO, buffer, total);
+	if (validate == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 	if (validate != total)
 	{
 		free(buffer);
