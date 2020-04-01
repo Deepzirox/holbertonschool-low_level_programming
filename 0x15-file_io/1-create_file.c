c747c489402ed44988e05ed4b81bc24cd32a27a7
Andres Camilo <1611@holbertonschool.com>
2020-03-31 19:35:00 -0500

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,21 +3,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
-/**
- * len - get lenght string
- * @txt: string
- * Return: counter
- */
-int len(char *txt)
-{
-	int c = 0;
-
-	while (txt[c] != '\0')
-	{
-		c++;
-	}
-	return (c);
-}
+#include <string.h>
 /**
  * create_file - create new file
  * @filename: name of file
@@ -35,12 +21,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 	{
-		write(STDOUT_FILENO, "Fails", 6);
 		return (-1);
 	}
 	if (text_content != NULL)
 	{
-		n = write(fd, text_content, len(text_content));
+		n = write(fd, text_content, strlen(text_content));
 		if (n == -1)
 		{
 			write(STDOUT_FILENO, "Fails", 6);
