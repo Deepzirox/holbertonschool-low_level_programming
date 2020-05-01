89bac694c04f363cf74267fd752f3565de88a943
deep <deep@localhost.localdomain>
2020-05-01 21:40:17 +0000

    add file 1

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_dlistint - get number of elements
+ * @h: head of list
+ * Return: Number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t elements = 0;
+
+	if (h == NULL)
+		return (elements);
+	while (h != NULL)
+	{
+		h = h->next;
+		elements++;
+	}
+	return (elements);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Directory for double linked list
