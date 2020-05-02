0ddedbd972a9d393f56db4a89f12f872adbf9314
deep <deep@localhost.localdomain>
2020-05-02 18:00:36 +0000

    add

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,22 +11,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
 
-	if (new == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	if (head == NULL)
-	{
-		new->n = n;
-		new->prev = NULL;
-		new->next = NULL;
-		*head = new;
-		return (new);
-	}
 	new->n = n;
+	new->next = (*head);
 	new->prev = NULL;
-	new->next = *head;
+
+	if ((*head) != NULL)
+		(*head)->prev = new;
 
 	*head = new;
 	return (new);
