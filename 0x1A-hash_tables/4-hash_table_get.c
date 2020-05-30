88771cd388e22e56cddff84b8339717abf7b7b3c
Camilo tobon <1611@holbertonschool.com>
2020-05-29 20:32:58 -0500

    add

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - get value of table by key
+ * @ht: hash table
+ * @key: key in hash table
+ * Return: Value if Success (null) if fail
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int k;
+
+	if (ht != NULL)
+		k = key_index((unchar)key, ht->size);
+	else
+		return (NULL);
+
+	if (ht->array[k] == NULL)
+		return ("(null)");
+
+	if (hash_djb2((unchar)ht->array[k]->key) == hash_djb2((unchar)key))
+		return (ht->array[k]->value);
+
+	return (NULL);
+}
