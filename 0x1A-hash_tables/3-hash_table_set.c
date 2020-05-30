bf46cada6f044e5d242a1f61343878175e5be16d
Camilo tobon <1611@holbertonschool.com>
2020-05-29 19:58:38 -0500

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -45,6 +45,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (new_node == NULL)
 		return (0);
+
+	if (key == NULL)
+		return (0);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 	{
